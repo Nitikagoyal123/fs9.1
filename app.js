c8880901c823719867554d8338b8e555863f23f7
nitika goyal <abhigoyal7866@gmail.com>
2025-10-31 11:09:43 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>🚀 React App Running in Docker!</h1>
+      <p>This React app is built and served using a multi-stage Docker build.</p>
+      <p>Frontend served by Nginx 🔥</p>
+    </div>
+  );
+}
+
+export default App;
